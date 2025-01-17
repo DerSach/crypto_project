a8b36d3aebc90649f9ffa5b17bac5fd7eadadefd
Sacha Lehmann <78410163+DerSach@users.noreply.github.com>
2025-01-17 20:38:02 +0100

    Add files via upload

--- crypto_project_v2.py
+++ crypto_project_v2.py
@@ -5,7 +5,7 @@ import os.path
 import pyinputplus as pyip
 import datetime
 
-def get_trades_and_portfolio_dfs():
+def get_trades_portfolio_and_stop_orders_dfs():
     ''' Upload our trades and portoflio csvs or create one if it does not exist '''
     if os.path.isfile('data/v2/crypto_trades.csv'):
         trades_df = pd.read_csv('data/v2/crypto_trades.csv', index_col=[0])
@@ -30,9 +30,21 @@ def get_trades_and_portfolio_dfs():
     portfolio_df.sort_values(by = 'crypto', inplace = True)
     portfolio_df.reset_index(inplace = True, drop  = True)
     
-    return trades_df, portfolio_df
+    ''' Same for stop orders dfs '''
+    if os.path.isfile('data/v2/crypto_stop_orders.csv'):
+        stop_orders_df = pd.read_csv('data/v2/crypto_stop_orders.csv', index_col=[0])
+        if list(stop_orders_df.columns) != ['date','crypto','b/s','stop_order_price','amount','currency','crypto_unit']:
+            print('Your stop orders csv does not have the right columns names. Please change it, remove it or move it to another folder and relaunch the program. ')
+            sys.exit(1)
+    else:
+        col_list = ['date','crypto','b/s','trade_price','amount','currency','crypto_unit']
+        stop_orders_df = pd.DataFrame(columns = col_list)
+    stop_orders_df.sort_values(by = 'date', inplace = True)
+    stop_orders_df.reset_index(inplace = True, drop  = True)
+    
+    return trades_df, portfolio_df, stop_orders_df
 
-def print_portfolios(trades_df, portfolio_df):
+def print_portfolios(trades_df, portfolio_df, stop_orders_df):
     ''' Displays your trades and current portfolio and, if it exists, your historical profit csv'''
     if len(trades_df) == 0:
         print('Your trades csv is empty')
@@ -48,6 +60,13 @@ def print_portfolios(trades_df, portfolio_df):
         print('YOUR CRYPTO PORTFOLIO')
         print(portfolio_df)
                 
+    if len(stop_orders_df) == 0:
+        print('Your stop market orders csv is empty')
+    else:
+        print()
+        print('YOUR CRYPTO STOP MARKET ORDERS')
+        print(stop_orders_df)
+        
     if os.path.isfile('data/v2/crypto_historical_profit.csv'):
         hist_profit_df = pd.read_csv('data/v2/crypto_historical_profit.csv', index_col=[0])
         print()
@@ -86,6 +105,38 @@ def add_crypto_trade():
     portfolio_df.to_csv('data/v2/crypto_portfolio.csv')
     return trades_df, portfolio_df
 
+
+def add_stop_order():
+    
+    ''' Create if need be our trades history csv file '''
+    if os.path.isfile('data/v2/crypto_stop_orders.csv'):
+        stop_orders_df = pd.read_csv('data/v2/crypto_stop_orders.csv', index_col=[0])
+        if list(stop_orders_df.columns) != ['date','crypto','b/s','stop_order_price','amount','currency','crypto_unit']:
+            return print('Cannot update your stop orders history csv as it has not the right columns names. Please change it, remove it or move it to another folder and relaunch the program. ')
+    else:
+        col_list = ['date','crypto','b/s','stop_order_price','amount','currency','crypto_unit']
+        stop_orders_df = pd.DataFrame(columns = col_list)
+        
+    ''' Add a stop order to your stop orders df '''
+    date = pyip.inputDate('Please enter date of trade (YYYY/mm/dd): ', formats=['%Y/%m/%d'])
+    crypto = pyip.inputStr('Please enter the crypto symbol traded (BTC, DOGE, ...): ')
+    buyorsell = pyip.inputChoice(['Buy', 'Sell'])
+    stop_order_price = pyip.inputNum('Please enter the stop market order price of the trade: ')
+    currency = pyip.inputStr('Please enter the currency symbol of your trade (EUR, USD, ...): ')
+    crypto_unit = pyip.inputNum('Please enter the number of crypto units that will be traded if this stop market order is executed: ')
+    if buyorsell == 'Buy':
+        amount = - float(crypto_unit) * float(stop_order_price)
+    elif buyorsell == 'Sell':
+        amount = float(crypto_unit) * float(stop_order_price)
+    list_x = [date, crypto.upper(), buyorsell, float(stop_order_price), float(amount), currency.upper(), float(crypto_unit)]
+    len_df = len(stop_orders_df)
+    stop_orders_df.loc[len_df] = list_x
+    if not os.path.isdir('data/v2'):
+        os.makedirs('data/v2')
+    stop_orders_df.to_csv('data/v2/crypto_stop_orders.csv')
+    return stop_orders_df
+
+
 def remove_crypto():
     ''' Removing a trade and updating the portfolio '''
     trades_df = pd.read_csv('data/v2/crypto_trades.csv', index_col=[0])
@@ -99,7 +150,21 @@ def remove_crypto():
         trades_df.to_csv('data/v2/crypto_trades.csv')
         portfolio_df = trades_df.groupby('crypto',as_index=False)['crypto_unit'].sum()
         portfolio_df.to_csv('data/v2/crypto_portfolio.csv')
-
+        
+        
+def remove_stop_order():
+    ''' Removing a stop market order '''
+    stop_orders_df = pd.read_csv('data/v2/crypto_stop_orders.csv', index_col=[0])
+    if len(stop_orders_df) == 0:
+        print('Your stop market order history is empty')
+    else:
+        print(stop_orders_df)
+        index_removed = pyip.inputNum('Which trade number do you want to remove? ')
+        stop_orders_df.drop(index = index_removed, inplace = True)
+        stop_orders_df.reset_index(drop=True, inplace = True)
+        stop_orders_df.to_csv('data/v2/crypto_stop_orders.csv')
+        
+        
 def get_trade_amount_target_currency(trades_df, target_currency, dic):
     ''' Get the amount for each trade (buy or sell) in the target currency'''
     
@@ -192,15 +257,16 @@ def get_historical_profit_df(profit, currency):
 
 def main():
     ''' Main function '''
-    choice = input('Do you want to [see] your portfolio, [add] a crypto trade to your portfolio, [remove] a crypto trade from your portfolio, or get your current [profit]? ')  
-    if choice not in ['see','add','remove','profit']:
-        print('Please enter either "see", "add", "remove" or "profit"')
+    choice = input('Do you want to [see] your portfolio, [add] a crypto trade to your portfolio, [remove] a crypto trade from your portfolio, get your current [profit], [add_stop] market order, or [remove_stop] market order? ')  
+    print("Remember to check your Stop Market orders")
+    if choice not in ['see','add','remove','profit','add_stop','remove_stop']:
+        print('Please enter either "see", "add", "remove", "profit", "add_stop" or "remove_stop"')
         sys.exit(1)
     else: 
-        trades_df, portfolio_df = get_trades_and_portfolio_dfs()
+        trades_df, portfolio_df, stop_orders_df = get_trades_portfolio_and_stop_orders_dfs()
         if choice == 'see':
-            print_portfolios(trades_df, portfolio_df)
-            print()
+            print_portfolios(trades_df, portfolio_df, stop_orders_df)
+            print()    
         if choice == 'add':
             trades_df, portfolio_df = add_crypto_trade()
             print('Crypto trade successfully added and portfolio updated!')
@@ -210,8 +276,16 @@ def main():
             print()
             print('Crypto trade successfully removed and portfolio updated!')
             print()
+        if choice == 'add_stop':
+            stop_orders_df = add_stop_order()
+            print('Crypto stop market order successfully added!')
+            print()
+        if choice == 'remove_stop':
+            remove_stop_order()
+            print('Crypto stop market order successfully removed!')
+            print()
         elif choice == 'profit':
-            trades_df, portfolio_df = get_trades_and_portfolio_dfs()
+            trades_df, portfolio_df, stop_orders_df = get_trades_portfolio_and_stop_orders_dfs()
             if len(trades_df) == 0:
                 print('Your trades history is empty! Please add crypto before computing your profit.')
             else:
